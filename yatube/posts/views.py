9e9c6880c396b6c185225d9099c6f7691224b8f7
XDreamsonX <sklyar10000@gmail.com>
2022-06-18 21:47:08 +0300

    'HW'

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,15 @@
-from django.http import HttpResponse
+# posts/views.py
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import Post, Group
 
 def index(request):
-    template = 'posts/index.html'
-    return render(request, template)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {'posts': posts}
+    return render(request, 'posts/index.html', context)
 
 def group_posts(request, slug):
-    return HttpResponse('Посты отфильтрованные по группам')
-# Create your views here.
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by("-pub_date")[:10]
+    context = {'group': group, 'posts': posts}
+    return render(request, 'posts/group_list.html', context)
